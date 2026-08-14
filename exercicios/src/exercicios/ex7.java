725950e2720679d68c326fdd0f4327ca0826783e
Mateus Rodrigues <matteus2204@gmail.com>
2026-08-14 09:23:45 -0300

    Adicionando comentários explicativos.

--- exercicios/src/exercicios/ex7.java
+++ exercicios/src/exercicios/ex7.java
@@ -14,22 +14,27 @@ public class ex7 {
 	public static void main(String[] args) {
 		
 		Scanner teclado = new Scanner (System.in);
+		//Cria os vetores e a variável do produto
 		int[] A = new int[3];
 		int[] B = new int[3];
 		int prod = 0;
 
+		//Pede e armazena as coordenadas do vetor A
 		for (int i = 0; i < 3; i++) {
 			System.out.println("Digite a coordena a" + (i+1) + " do vetor A: ");
 			A[i] = teclado.nextInt();
 		}
+		//Pede e armazena as coordenadas do vetor B
 		for (int j = 0; j < 3; j++) {
 			System.out.println("Digite a coordena b" + (j+1) + " do vetor B: ");
 			B[j] = teclado.nextInt();
 		}
+		//Calcula e armazena o produto escalalar entre A e B
 		for (int k = 0; k < 3; k++) {
+			//Incrementa em prod a multiplicação de duas coordenadas correspondentes
 			prod += A[k] * B[k];
 		}
-		
+		//Exibe o resultado
 		System.out.println("A*B = " + prod);
 		
 		teclado.close();
