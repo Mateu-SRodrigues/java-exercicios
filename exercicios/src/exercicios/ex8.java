0b8ed24976ebb6bbb314e0d8c85e755a9b0167de
Mateus Rodrigues <matteus2204@gmail.com>
2026-08-14 09:37:40 -0300

    Exercício 8: números ímpares e pares.

---
+++ exercicios/src/exercicios/ex8.java
@@ -0,0 +1,31 @@
+/*8. Em uma partida de RPG, o mestre está determinando o resultado 
+ * de uma batalha para um jogador usando um dado. Escreva um algoritmo 
+ * que receba um número  inteiro como entrada e, com base nesse número, 
+ * determine se o jogador ganhou ou  perdeu a batalha. Se o número for par, 
+ * o jogador vence; se for ímpar, o jogador perde.*/
+
+package exercicios;
+import java.util.Scanner;
+
+public class ex8 {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner (System.in);
+		
+		System.out.println("Digite o nome do jogador: ");
+		String nome = teclado.next();
+		System.out.println("Digite o número do jogador: ");
+		int n = teclado.nextInt();
+		
+		if (n % 2 == 0) {
+			System.out.println("O jogador " + nome + " VENCEU!");
+		}
+		else {
+			System.out.println("O jogador " + nome + " PERDEU!");
+		}
+		
+		teclado.close();
+	}
+
+}
